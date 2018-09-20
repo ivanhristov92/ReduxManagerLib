3f0e9f885ea9b171f9f1be5fce1e825271067ea6
ivanhristov92 <ivan_hristov@mail.bg>
2018-09-20 18:10:09 +0300

    prototyping the reducer factory

--- crud-reducer.js
+++ crud-reducer.js
@@ -1,3 +1,35 @@
 export default function reducerFactory() {
-  return function reducer(state, action) {};
+  return function reducer(state = { byId: {}, isFetching: false }, action) {
+    switch (action) {
+      case "CREATE":
+      case "READ":
+      case "UPDATE":
+      case "DELETE":
+        return {
+          ...state,
+          isFetching: true
+        };
+      case "CREATE__SUCCESS":
+      case "READ__SUCCESS":
+      case "UPDATE__SUCCESS":
+        return {
+          ...state,
+          byId: {
+            ...state.byId,
+            ...action.payload.byId
+          },
+          isFetching: false
+        };
+      case "CREATE__FAILURE":
+      case "READ__FAILURE":
+      case "UPDATE__FAILURE":
+        return {
+          ...state,
+          isFetching: false
+        };
+
+      default:
+        return state;
+    }
+  };
 }
