fee5badd62362fa2986c79fe36d7699133c80000
ivanhristov92 <ivan_hristov@mail.bg>
2018-09-20 17:32:39 +0300

    working on the rest api layer

---
+++ crud-rest-api.js
@@ -0,0 +1,8 @@
+export default function restApiFactory() {
+  return {
+    create() {},
+    read() {},
+    update() {},
+    delete() {}
+  };
+}
---
+++ test/crud-rest-api.js
@@ -0,0 +1,40 @@
+import restApiFactory from "../crud-rest-api";
+var assert = require("assert");
+var sinon = require("sinon");
+import * as _ from "ramda";
+
+describe("CRUD Rest Api", () => {
+  describe("The module must expose a factory function, that creates the rest api functions for a model", function() {
+    it("The module should default export a function", () => {
+      assert.equal(typeof restApiFactory, "function");
+    });
+  });
+
+  describe("The factory function must return an object with the crud rest calls", function() {
+    it("returns an object", () => {
+      let restApi = restApiFactory();
+      assert.equal(typeof restApi, "object");
+    });
+
+    it("returns a non-empty object", () => {
+      let restApi = restApiFactory({}, {}, {});
+      assert.equal(_.isEmpty(restApi), false);
+    });
+
+    describe("The factory function must return an object with all crud rest calls", function() {
+      const propNotFound = " property was not found";
+
+      ["create", "read", "update", "delete"].forEach(crudAct => {
+        it(`the factory returns an object with a '${crudAct}' function`, () => {
+          let restApi = restApiFactory();
+          assert.equal(
+            _.has(crudAct, restApi),
+            true,
+            `${crudAct} ${propNotFound}`
+          );
+          assert.equal(typeof restApi[crudAct], "function");
+        });
+      });
+    });
+  });
+});
