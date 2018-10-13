a3ee104e47d0e39833e718e2397468e303915f02
ivanhristov92 <ivan_hristov@mail.bg>
2018-10-13 15:12:00 +0300

    refactoring custom error type tests

--- test/crud-error-types.js
+++ test/crud-error-types.js
@@ -2,14 +2,14 @@ var assert = require("assert");
 import * as errorTypes from "../crud-error-types";
 
 describe("CRUD Error Types", () => {
-  it("The module should export an error of type 'ModuleInitializationTypeError'", () => {
+  it("[EXPORTS] The module should export an error of type 'ModuleInitializationTypeError'", () => {
     assert.notEqual(
       typeof errorTypes.ModuleInitializationTypeError,
       "undefined"
     );
   });
 
-  it("The module should export an error of type 'UnexpectedRuntimeError'", () => {
+  it("[EXPORTS] The module should export an error of type 'UnexpectedRuntimeError'", () => {
     assert.notEqual(typeof errorTypes.UnexpectedRuntimeError, "undefined");
   });
 });
