c92f5c40f859f1f77c6c28e74e46eca74dfc5e0b
ivanhristov92 <ivan_hristov@mail.bg>
2018-10-27 12:42:51 +0300

    working on selectors

--- crud-selectors.js
+++ crud-selectors.js
@@ -1,11 +1,16 @@
-import { dispatchAnUnexpectedErrorEvent } from "./crud-error-types";
+import {
+  dispatchAnUnexpectedErrorEvent,
+  ModuleInitializationTypeError
+} from "./crud-error-types";
 
 export default function selectorsFactory(options) {
+  typeCheckOptions(options);
+
   let runtimeErrorHandler =
     (options && options.customErrorHandler) || defaultRuntimeErrorHandler;
 
   function defaultRuntimeErrorHandler(error, { state, action }) {
-      dispatchAnUnexpectedErrorEvent(error, { state, action });
+    dispatchAnUnexpectedErrorEvent(error, { state, action });
   }
 
   function getAll(state, asArray) {
@@ -13,25 +18,25 @@ export default function selectorsFactory(options) {
       return asArray ? Object.values(state.byId) : state.byId;
     } catch (error) {
       runtimeErrorHandler(error, { state, asArray, selector: "getAll" });
-      return asArray ? [] || {};
+      return asArray ? [] : {};
     }
   }
 
   function getOne(state, id) {
     try {
-        return state.byId[id];
+      return state.byId[id];
     } catch (error) {
-        runtimeErrorHandler(error, { state, id, selector: "getOne" });
-        return {}
+      runtimeErrorHandler(error, { state, id, selector: "getOne" });
+      return {};
     }
   }
 
   function getSome(state, ids = []) {
     try {
-        return ids.map(id => state.byId[id]);
+      return ids.map(id => state.byId[id]);
     } catch (error) {
-        runtimeErrorHandler(error, { state, ids, selector: "getSome" });
-        return [];
+      runtimeErrorHandler(error, { state, ids, selector: "getSome" });
+      return [];
     }
   }
 
@@ -42,4 +47,31 @@ export default function selectorsFactory(options) {
   };
 }
 
+function typeCheckOptions(options) {
+  if (!isOptionalObject(options)) {
+    throw new ModuleInitializationTypeError(
+      `Expected an object or undefined, instead received ${typeof options}: ${options}`
+    );
+  }
 
+  if (
+    isObject(options) &&
+    typeof options.customErrorHandler !== "undefined" &&
+    typeof options.customErrorHandler !== "function"
+  ) {
+    throw new ModuleInitializationTypeError(
+      `Expected an 'customErrorHandler' to be a function, instead received ${typeof options.customErrorHandler}: ${
+        options.customErrorHandler
+      }`
+    );
+  }
+}
+
+// helpers
+function isObject(value) {
+  return typeof value === "object" && !Array.isArray(value) && value;
+}
+
+function isOptionalObject(value) {
+  return typeof value === "undefined" || isObject(value);
+}
--- test/crud-selectors.js
+++ test/crud-selectors.js
@@ -1,6 +1,7 @@
 import selectorsFactory from "../crud-selectors";
 var assert = require("assert");
 import * as _ from "ramda";
+import actionCreatorsFactory from "../crud-action-creators";
 
 describe("CRUD SELECTORS", () => {
   describe("[EXPORTS]", () => {
@@ -10,16 +11,63 @@ describe("CRUD SELECTORS", () => {
   });
 
   describe("[MODULE INITIALIZATION SIGNATURE]", () => {
-    describe("[RETURNS] an object with selectors", () => {
-      it("[CORRECT TYPE] returns an object", () => {
-        assert.equal(typeof selectorsFactory(), "object");
-        assert.equal(Array.isArray(selectorsFactory()), false);
-        assert.notEqual(selectorsFactory(), null);
+    describe("selectorsFactory", () => {
+      describe("[EXPECTS] an options object", () => {
+        describe("[ACCEPTS] does not throw, when passed an options object", () => {
+          it("[CORRECT TYPE] {}", () => {
+            assert.doesNotThrow(function() {
+              selectorsFactory({});
+            }, Error);
+          });
+          it("[CORRECT TYPE] undefined", () => {
+            assert.doesNotThrow(function() {
+              selectorsFactory(undefined);
+            }, Error);
+          });
+
+          it("[CORRECT TYPE] options.customErrorHandler function", () => {
+            assert.doesNotThrow(function() {
+              selectorsFactory({ customErrorHandler: function() {} });
+            }, Error);
+          });
+        });
+
+        describe("[THROWS] a ModuleInitializationTypeError", () => {
+          [null, false, true, Error, function() {}, []].forEach(value => {
+            it(`[THROWS] when passed a ${typeof value}: ${value}`, () => {
+              try {
+                selectorsFactory(value);
+                assert.equal(false, true, "Should have thrown");
+              } catch (err) {
+                assert.equal(err.name, "ModuleInitializationTypeError");
+              }
+            });
+          });
+
+          [null, false, true, "", "test", []].forEach(value => {
+            it(`[THROWS] when customErrorHandler is not a function, but a ${typeof value}: ${value}`, () => {
+              try {
+                selectorsFactory({ customErrorHandler: value });
+                assert.equal(false, true, "Should have thrown");
+              } catch (err) {
+                assert.equal(err.name, "ModuleInitializationTypeError");
+              }
+            });
+          });
+        });
       });
 
-      ["getOne", "getSome", "getAll"].forEach(method => {
-        it(`[CORRECT TYPE] has a ${method} function`, () => {
-          assert.equal(typeof selectorsFactory()[method], "function");
+      describe("[RETURNS] an object with selectors", () => {
+        it("[CORRECT TYPE] returns an object", () => {
+          assert.equal(typeof selectorsFactory(), "object");
+          assert.equal(Array.isArray(selectorsFactory()), false);
+          assert.notEqual(selectorsFactory(), null);
+        });
+
+        ["getOne", "getSome", "getAll"].forEach(method => {
+          it(`[CORRECT TYPE] has a ${method} function`, () => {
+            assert.equal(typeof selectorsFactory()[method], "function");
+          });
         });
       });
     });
@@ -30,7 +78,10 @@ describe("CRUD SELECTORS", () => {
       describe(method, () => {
         describe("[EXPECTS]", () => {
           describe("state as 0th argument", () => {
-            it("[ACCEPTS] an object", () => {});
+            it("[ACCEPTS] an object", () => {
+              const selectors = selectorsFactory();
+              selectors.getSome();
+            });
             it("[THORWS][HANDLES]", () => {});
           });
         });
