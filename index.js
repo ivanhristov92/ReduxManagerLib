ba83ee1a70a885bdbdd907b00b287eb68a836494
ivanhristov92 <ivan_hristov@mail.bg>
2018-10-27 20:18:03 +0300

    selectors

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import actionTypesFactory from "./crud-action-types";
 import actionCreatorsFactory from "./crud-action-creators";
 import reducerFactory from "./crud-reducer";
+import selectorsFactory from "./crud-selectors";
 
 export { actionTypesFactory };
 export { actionCreatorsFactory };
 export { reducerFactory };
+export { selectorsFactory };
 
 import { attachAnUnexpectedErrorLogger } from "./crud-error-types";
 export { attachAnUnexpectedErrorLogger };
