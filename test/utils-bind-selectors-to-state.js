a48e42b1735a5c3dfaf0e46522905f596ed66856
ivanhristov92 <ivan_hristov@mail.bg>
2018-11-04 15:31:56 +0200

    testing utils

--- test/utils-bind-selectors-to-state.js
+++ test/utils-bind-selectors-to-state.js
@@ -1,4 +1,5 @@
 import { bindSelectorsToState } from "../utils/bind-selectors-to-state";
+import actionCreatorsFactory from "../crud-action-creators";
 var assert = require("assert");
 var sinon = require("sinon");
 
@@ -10,7 +11,34 @@ describe("CRUD Action Creators", () => {
   });
 
   describe("[RUNTIME SIGNATURE]", function() {
-    describe("[EXPECTS]", function() {});
+    describe("[EXPECTS]", function() {
+      describe("[0] subStateGetter - non-empty string or a function", () => {
+        it(`[ACCEPTS][CORRECT TYPE] a non-empty string`, () => {
+          assert.doesNotThrow(function() {
+            bindSelectorsToState("non-empty", {});
+          });
+        });
+
+        it(`[ACCEPTS][CORRECT TYPE] a function`, () => {
+          assert.doesNotThrow(function() {
+            bindSelectorsToState(function() {}, {});
+          });
+        });
+
+        [[], "", null, false, true, 100, 0, {}].forEach((value, index) => {
+          it(`${index} [THROWS] throws a ModuleInitializationTypeError if 'subStateGetter' is not a string or a function , but ${typeof value} : ${value}`, () => {
+            try {
+              bindSelectorsToState(value, {});
+              throw new Error("should have thrown");
+            } catch (err) {
+              assert.equal(err.name, "ModuleInitializationTypeError");
+            }
+          });
+        });
+      });
+
+      describe("[1] selectors", () => {});
+    });
     describe("[RETURNS]", function() {});
   });
 
