1871dad6d16f01a08f1a9a9d44a870ef14d34bfe
ivanhristov92 <ivan_hristov@mail.bg>
2018-11-05 19:57:59 +0200

    adding webpack for building

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require("path");
+const commonPaths = {
+  root: path.resolve(__dirname),
+  outputPath: path.resolve(__dirname, "build"),
+  entryPath: path.resolve(__dirname, "index.js"),
+  jsFolder: "js"
+};
+
+module.exports = {
+  mode: "production",
+  entry: commonPaths.entryPath,
+  output: {
+    // filename: `${commonPaths.jsFolder}/[name].[hash].js`,
+    path: commonPaths.outputPath,
+    chunkFilename: "[name].[chunkhash].js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        loader: "babel-loader",
+        exclude: /(node_modules)/
+      }
+    ]
+  },
+
+  resolve: {
+    modules: ["src", "node_modules"],
+    extensions: ["*", ".js", ".jsx", ".css", ".scss"]
+  },
+  plugins: []
+};
