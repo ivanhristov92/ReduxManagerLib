c360865b0278f9c4448ceb96fb53697b181d0119
ivanhristov92 <ivan_hristov@mail.bg>
2018-11-09 11:52:31 +0200

    fixing an issue with flow

--- crud-rest-api.flow.js
+++ crud-rest-api.flow.js
@@ -1,10 +1,10 @@
 // @flow
 export type NormalizedData<T> = {
-    [id: string | number]: T
+  [id: string | number]: T
 };
 
 export type RMLNormalizedDataInWrapper<T> = {
-    byId: NormalizedData<T>
+  byId: NormalizedData<T>
 };
 
 export type RMLPayloadExpectedByRestClient = any;
@@ -13,41 +13,42 @@ export type RMLPayloadExpectedByRestClient = any;
  * General Types
  */
 export type RMLRestClient = {
-    create: (
-        payload: RMLPayloadExpectedByRestClient
-    ) => Promise<RMLNormalizedDataInWrapper<any> | Error>,
-    read: (
-        payload: RMLPayloadExpectedByRestClient
-    ) => Promise<RMLNormalizedDataInWrapper<any> | Error>,
-    update: (
-        payload: RMLPayloadExpectedByRestClient
-    ) => Promise<RMLNormalizedDataInWrapper<any> | Error>,
-    delete: (
-        payload: RMLPayloadExpectedByRestClient
-    ) => Promise<{ ids: Array<string | number> } | { id: string | number } | Error>,
-    [additionalMethod: string]: (payload: any) => Promise<any | Error>
+  create: (
+    payload: RMLPayloadExpectedByRestClient
+  ) => Promise<RMLNormalizedDataInWrapper<any> | (Error | Object)>,
+  read: (
+    payload: RMLPayloadExpectedByRestClient
+  ) => Promise<RMLNormalizedDataInWrapper<any> | (Error | Object)>,
+  update: (
+    payload: RMLPayloadExpectedByRestClient
+  ) => Promise<RMLNormalizedDataInWrapper<any> | (Error | Object)>,
+  delete: (
+    payload: RMLPayloadExpectedByRestClient
+  ) => Promise<
+    { ids: Array<string | number> } | { id: string | number } | (Error | Object)
+  >,
+  [additionalMethod: string]: (payload: any) => Promise<any | (Error | Object)>
 };
-
 
 /**
  * Instance Types
  */
 export type RMLCreate<ExpectsPayload, EntryShape, ErrorShape> = (
-    payload: ExpectsPayload
+  payload: ExpectsPayload
 ) => Promise<RMLNormalizedDataInWrapper<EntryShape> | ErrorShape>;
 
 export type RMLRead<ExpectsPayload, EntryShape, ErrorShape> = (
-    payload: ExpectsPayload
-) => Promise<RMLNormalizedDataInWrapper<EntryShape> | ErrorShape>
+  payload: ExpectsPayload
+) => Promise<RMLNormalizedDataInWrapper<EntryShape> | ErrorShape>;
 
-export type RMLUpdate <ExpectsPayload, EntryShape, ErrorShape> = (
-    payload: ExpectsPayload
-) => Promise<RMLNormalizedDataInWrapper<EntryShape> | ErrorShape>
+export type RMLUpdate<ExpectsPayload, EntryShape, ErrorShape> = (
+  payload: ExpectsPayload
+) => Promise<RMLNormalizedDataInWrapper<EntryShape> | ErrorShape>;
 
 export type RMLDelete<ExpectsPayload, IdType, ErrorShape> = (
-    payload: ExpectsPayload
-) => Promise<{ ids: Array<IdType> } | { id: IdType } | ErrorShape>
+  payload: ExpectsPayload
+) => Promise<{ ids: Array<IdType> } | { id: IdType } | ErrorShape>;
 
 export type RMLAdditionalMethod<ExpectsPayload, ReturnsShape, ErrorShape> = (
-    payload: ExpectsPayload
-) => Promise<ReturnsShape | ErrorShape>
+  payload: ExpectsPayload
+) => Promise<ReturnsShape | ErrorShape>;
