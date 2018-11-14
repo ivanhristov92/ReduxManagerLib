c9dc51bd8d073cfa02a496f26fc6babec81e07b2
ivanhristov92 <ivan_hristov@mail.bg>
2018-11-14 13:34:17 +0200

    making crud-rest-client tests available to the user

--- test/crud-rest-api.js
+++ test/crud-rest-api.js
@@ -1,98 +1,102 @@
-import restApiFactory from "../crud-rest-api";
 var assert = require("assert");
-import * as _ from "ramda";
+let _ = require("ramda");
 
-describe("CRUD Rest Api", () => {
-  describe("[EXPORTS INTERFACE] The module must expose a factory function, that creates the rest api functions for a model", function() {
-    describe("[EXPORTS A FACTORY]", function() {
-      it("The module should default export a function", () => {
-        assert.equal(typeof restApiFactory, "function");
-      });
-    });
-  });
+module.exports = function testRestClientInstance(restClient) {
+  let restApiFactory =
+    typeof restClient === "function" ? restClient : () => restClient;
 
-  describe("[MODULE INITIALIZATION SIGNATURE]", () => {
-    describe("factory", () => {
-      describe("[RETURNS]", () => {
-        it("[CORRECT TYPE] returns an object", () => {
-          let restApi = restApiFactory();
-          assert.equal(typeof restApi, "object");
-        });
-
-        it("[CORRECT TYPE] returns a non-empty object", () => {
-          let restApi = restApiFactory({}, {}, {});
-          assert.equal(_.isEmpty(restApi), false);
-        });
-
-        ["create", "read", "update", "delete"].forEach(crudAct => {
-          const propNotFound = " property was not found";
-
-          it(`[CORRECT TYPE] the factory returns an object with a '${crudAct}' function`, () => {
-            let restApi = restApiFactory();
-            assert.equal(
-              _.has(crudAct, restApi),
-              true,
-              `${crudAct} ${propNotFound}`
-            );
-            assert.equal(typeof restApi[crudAct], "function");
-          });
+  describe("CRUD Rest Api", () => {
+    describe("[EXPORTS INTERFACE] The module must expose a factory function, that creates the rest api functions for a model", function() {
+      describe("[EXPORTS A FACTORY]", function() {
+        it("The module should default export a function", () => {
+          assert.equal(typeof restApiFactory, "function");
         });
       });
     });
-  });
 
-  describe("[RUNTIME SIGNATURE]", () => {
-    /*
-    describe("create", function() {
-      describe("[RETURNS]", () => {
-        describe("[CORRECT TYPE]", () => {
-          it(`the 'create' function must return a Promise`, () => {
+    describe("[MODULE INITIALIZATION SIGNATURE]", () => {
+      describe("factory", () => {
+        describe("[RETURNS]", () => {
+          it("[CORRECT TYPE] returns an object", () => {
             let restApi = restApiFactory();
-            let promise = restApi.create([{ test: 1 }]);
+            assert.equal(typeof restApi, "object");
+          });
 
-            assert.equal(
-              typeof promise.then,
-              "function",
-              `$create does not return a Promise`
-            );
+          it("[CORRECT TYPE] returns a non-empty object", () => {
+            let restApi = restApiFactory({}, {}, {});
+            assert.equal(_.isEmpty(restApi), false);
+          });
+
+          ["create", "read", "update", "delete"].forEach(crudAct => {
+            const propNotFound = " property was not found";
+
+            it(`[CORRECT TYPE] the factory returns an object with a '${crudAct}' function`, () => {
+              let restApi = restApiFactory();
+              assert.equal(
+                _.has(crudAct, restApi),
+                true,
+                `${crudAct} ${propNotFound}`
+              );
+              assert.equal(typeof restApi[crudAct], "function");
+            });
           });
         });
+      });
+    });
 
-        describe(`[CORRECT TYPE] the resolved data must be normalized`, () => {
-          it("[CORRECT VALUE] should return normalized data with 1 entry when 1 object is created", done => {
-            let restApi = restApiFactory();
+    describe("[RUNTIME SIGNATURE]", () => {
+      /*
+            describe("create", function() {
+              describe("[RETURNS]", () => {
+                describe("[CORRECT TYPE]", () => {
+                  it(`the 'create' function must return a Promise`, () => {
+                    let restApi = restApiFactory();
+                    let promise = restApi.create([{ test: 1 }]);
 
-            restApi.create([{ test: 1 }]).then(response => {
-              assert.equal(typeof response.byId, "object");
-              assert.equal(_.keys(response.byId).length, 1);
-              done();
-            });
-          });
-          it("[CORRECT VALUE] should return normalized data with 2 entry when 2 object is created", done => {
-            let restApi = restApiFactory();
+                    assert.equal(
+                      typeof promise.then,
+                      "function",
+                      `$create does not return a Promise`
+                    );
+                  });
+                });
 
-            restApi.create([{ test: 1 }, { test: 2 }]).then(response => {
-              assert.equal(typeof response.byId, "object");
-              assert.equal(_.keys(response.byId).length, 2);
-              done();
-            });
-          });
-          it("[CORRECT VALUE] should return normalized data with 3 entry when 3 object is created", done => {
-            let restApi = restApiFactory();
+                describe(`[CORRECT TYPE] the resolved data must be normalized`, () => {
+                  it("[CORRECT VALUE] should return normalized data with 1 entry when 1 object is created", done => {
+                    let restApi = restApiFactory();
 
-            restApi
-              .create([{ test: 1 }, { test: 2 }, { test: 3 }])
-              .then(response => {
-                assert.equal(typeof response.byId, "object");
-                assert.equal(_.keys(response.byId).length, 3);
-                done();
+                    restApi.create([{ test: 1 }]).then(response => {
+                      assert.equal(typeof response.byId, "object");
+                      assert.equal(_.keys(response.byId).length, 1);
+                      done();
+                    });
+                  });
+                  it("[CORRECT VALUE] should return normalized data with 2 entry when 2 object is created", done => {
+                    let restApi = restApiFactory();
+
+                    restApi.create([{ test: 1 }, { test: 2 }]).then(response => {
+                      assert.equal(typeof response.byId, "object");
+                      assert.equal(_.keys(response.byId).length, 2);
+                      done();
+                    });
+                  });
+                  it("[CORRECT VALUE] should return normalized data with 3 entry when 3 object is created", done => {
+                    let restApi = restApiFactory();
+
+                    restApi
+                      .create([{ test: 1 }, { test: 2 }, { test: 3 }])
+                      .then(response => {
+                        assert.equal(typeof response.byId, "object");
+                        assert.equal(_.keys(response.byId).length, 3);
+                        done();
+                      });
+                  });
+                });
               });
-          });
-        });
-      });
+            });
+            */
     });
-    */
-  });
 
-  describe("[OPERATION]", () => {});
-});
+    describe("[OPERATION]", () => {});
+  });
+};
